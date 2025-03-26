19b1f6e0377fda2d747ed0e4cbde176a7d9d7e4b
Kirk <ksahdo10@gmail.com>
2025-03-26 15:39:17 -0400

    chore: add prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,8 @@ import globals from "globals";
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
-
+import nextPlugin from "@next/eslint-plugin-next"
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -12,13 +12,24 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"] },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  eslintConfigPrettier,
   {
-
     rules: {
       "react/react-in-jsx-scope": "off",
-      "react-hooks/exhaustive-deps": "warn",
       "react/prop-types": "off",
       "@typescript-eslint/explicit-module-boundary-types": "off"
     },
-  }
+  },
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+  {
+    ignores: ['.next/*'],
+  },
 ]);
