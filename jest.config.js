5c6b6eccd3338706a7d3023994fe6131cd84edec
Kirk <ksahdo10@gmail.com>
2025-04-02 16:39:34 -0400

    fix: remove stories files from jest test

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/app/**'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/app/**',
+    '!src/**/stories.tsx'
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
   transform: {
