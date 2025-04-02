235aa0f7eb86e4e89cea109821477ef854eb90cd
Kirk <ksahdo10@gmail.com>
2025-04-02 16:01:21 -0400

    chore: config storybook with tailwind

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,6 +1,6 @@
 export const Main = () => {
   return (
-    <main>
+    <main className="flex flex-col items-center justify-center bg-[#06092b] text-white w-full h-h-full p-12 ">
       <h1>React</h1>
     </main>
   )
